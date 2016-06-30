263e03a1524ed5ff207fc8122bc53c01336f8f24
Harald Rudell <harald@attiresoft.com>
2016-06-30 16:14:58 -0700

    Initial commit

---
+++ src/restserver.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import {EventEmitter} from 'events'
+
+export class RestServer extends EventEmitter{
+  constructor(o) {
+    super()
+console.log(this.constructor.name, 'constructor pid:', process.pid)
+    this.emitError = this.emitError.bind(this)
+    new Promise((resolve, reject) => {
+      // 160630 express@4.14.0
+      // the express default export is a single function named createApplication of type Function
+      console.log(this.constructor.name, 'express default export function name:', express.name)
+      // app is an anonymous function of type EventEmitter
+      var app = express()
+        .use(express.static('public')) // middleware is function (req, res, next)
+      // app.listen() returns a Server object
+      var server = app.listen(o.port, o.address, this.handleRequest.bind(this))
+    })
+    .catch(e => process.nextTick(this.emitError, e))
+  }
+
+  handleRequest() {
+console.log(this.constructor.name, 'handleRequest')
+  }
+
+  emitError(e) {
+console.log(this.constructor.name, 'ERROR')
+    this.emit('error', e)
+  }
+}

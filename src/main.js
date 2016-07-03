0be69e0e09d78eb0376c23b487449d4c115af7bc
Harald Rudell <harald@attiresoft.com>
2016-07-02 18:06:16 -0700

    ready for github

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 /*
+© 2016 Harald Rudell <c@haraldrudell.com> (http://haraldrudell.com) ISC license.
+
 webpack will invoke this code with:
 this value being the exports object, ie. an empty object literal
 and arguments:
@@ -9,8 +11,8 @@ a webpack require function
 The wrapping function is anonymous and part of a JavaScript expression,
 ie. it is not initially referenced from the global scope
 */
-// const is EcmaScript 2015, so if this works, that means that babel works.
-const x = 'EcmaScript 2015 parsing ok'
+// const is EcmaScript 2016, so if this works, that means that babel works.
+const x = 'EcmaScript 2016 parsing ok'
 console.log(x)
 
 // update a visible element in the page to prove that JavaScript is working.
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,28 @@
+/*
+© 2016 Harald Rudell <c@haraldrudell.com> (http://haraldrudell.com) ISC license.
+*/
 var path = require('path')
 var webpack = require('webpack')
 
-/*
-webpack expects a single object to be exported
-*/
 module.exports = {
-
-  entry: [ // build a bundle by gathering dependencies from these entry points
-    'babel-polyfill', // emulates a full ES2015 environment
-    './src/main' // the JavaScript to be executed by index.html
-    // the last item is exported, ie. accessible by other bundles or web resources
+  entry: [
+    'babel-polyfill',
+    './src/main'
   ],
-
-  // context: an absolute path for resolving entry items, default process.cwd()
-
   output: {
-    publicPath: '/', // public URI folder where the bundle filename is available to a browser
-    filename: 'main.js' // the filename of the bundle in the browser
+    publicPath: '/',
+    filename: 'main.js'
   },
-
-  devtool: 'source-map', // this displays the source code when publicPath/filename is accessed in the browser
-  // the source map for /main.js is /main.js.map
-
+  devtool: 'source-map',
   module: {
-
-    loaders: [{ // A loader transforms resources
-      test: /\.js$/, // any file ending in .js
-      include: path.join(__dirname, 'src'), // located in the project folder tree src
-      loader: 'babel-loader', // transform using babel
+    loaders: [{
+      test: /\.js$/,
+      include: path.join(__dirname, 'src'),
+      loader: 'babel-loader',
       query: {
-        presets: ['es2015'], // build qury string parameter arguments for the loader
-        // this is overriden if there is a .babelrc
+        presets: ['es2015', 'es2016'],
       }
     }]
   },
-
-  debug: true // Switch loaders to debug mode. (?)
-
+  debug: true
 }
